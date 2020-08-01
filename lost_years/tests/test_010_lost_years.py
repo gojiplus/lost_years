392d75df0709a4676b58692fb34fbb9fe9b95dc7
Suriyan Laohaprapanon <suriyant@gmail.com>
2020-08-01 02:51:01 +0000

    add a simple test case for WHO

--- lost_years/tests/test_010_lost_years.py
+++ lost_years/tests/test_010_lost_years.py
@@ -9,7 +9,7 @@ import os
 import shutil
 import unittest
 import pandas as pd
-from lost_years import lost_years_ssa, lost_years_hld
+from lost_years import lost_years_ssa, lost_years_hld, lost_years_who
 from . import capture
 
 
@@ -30,6 +30,12 @@ class TestInRollsFn(unittest.TestCase):
         odf = lost_years_hld(self.df)
         self.assertIn('hld_life_expectancy', odf.columns)
         self.assertTrue(odf.iloc[0].hld_year1 == 2003)
+
+    def test_lost_years_who(self):
+        odf = lost_years_who(self.df)
+        self.assertIn('who_life_expectancy', odf.columns)
+        self.assertTrue(odf.iloc[0].who_year == 2003)
+
 
 if __name__ == '__main__':
     unittest.main()
